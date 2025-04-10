008de7de4bf22b09c21f4d7c6513b97b75bbd634
sleepy-cat609 <bryan465xd@gmail.com>
2025-04-09 23:38:33 -0500

    Hola_mundo.java

--- hola_mundo/src/hola_mundo/Hola_mundo.java
+++ hola_mundo/src/hola_mundo/Hola_mundo.java
@@ -5,6 +5,7 @@ package hola_mundo;
 public class Hola_mundo {
     public static void main(String[] args) {
         System.out.println("Hola..........");
+        System.out.println("Adios.........");
     }
     
 }
